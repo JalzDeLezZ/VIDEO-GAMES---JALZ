3a81245fe07914d0383d5f8f647327fdb67c4327
JalzDeLezZ <j4mes.delez@gmail.com>
2022-04-04 23:21:40 -0500

    Vilculate Order And Filters ASC DSC

--- client/src/components/home/subcomponents/filter.jsx
+++ client/src/components/home/subcomponents/filter.jsx
@@ -1,4 +1,4 @@
-import React, {  useEffect, useRef } from 'react'
+import React, {  useEffect, useRef, useState } from 'react'
 import SelectGroup from '../../elements/SelectGroup';
 import styled from "styled-components";
 import { useDispatch, useSelector } from 'react-redux'
@@ -18,6 +18,13 @@ const Filter = () => {
   let aGenres = reducer_genres.map(pI => pI.name);
   aGenres.unshift('All');//aGenres = ['All', ...aGenres]
   const x = useRef();
+
+  
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
   //FILTRO SELECT X' VALUE => DISPATCH
   const mOnChangeSelect = (e) => {
     const {value} = e.target; console.log(value);
@@ -29,20 +36,25 @@ const Filter = () => {
   }
 
   //FILTER RADIO BUTTONS X' VALUE => DISPATCH
-  // const [sRadioBtn , setSRadioBtn] = useState(null)
-  const mOnClickRbtOrder = (e) =>{
-    const {value} = e.target;  console.log(value);
-    // setSRadioBtn(value);
-    xDispatch_action(OrderFilter(value))
-  }
-  //
   const mOnChangeSelectData = (e) => {
     const {value} = e.target;  console.log(value);
     xDispatch_action(getAllVideoGames(value));
   }
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  const [typeOrder, setTipeOrder] = useState('NONE');
+  const [filterAscDsc, setFilterAscDsc] = useState('NONE');
+  const mTypeOrder = (e)=> {
+    const {value} = e.target; //console.log(value);
+    setTipeOrder(value);
+    xDispatch_action(OrderFilterAscDsc(value, filterAscDsc))
+  }
   const mOrderAscDsc = (e)=> {
-    const {value} = e.target;  console.log(value);
-    xDispatch_action(OrderFilterAscDsc("DAD", "SOON"))
+    const {value} = e.target; // console.log(value);
+    setFilterAscDsc(value);
+    xDispatch_action(OrderFilterAscDsc(typeOrder, value))
   }
   return (
     <MyDiv>
@@ -67,7 +79,7 @@ const Filter = () => {
           pLabel = "ORDEN"
           pName = "n_order"
           pAoptions= {["NONE","ALPHABETIC", "RATING"]}
-          pMReloadReducer = {mOnChangeSelect}
+          pMReloadReducer = {mTypeOrder}
         />
 
         <MySection>
@@ -76,7 +88,7 @@ const Filter = () => {
             pId="iAsc" 
             pLabel="Ord. Asc" 
             pValue="ASC" 
-            pMOnClickRbt={mOnClickRbtOrder}
+            pMOnClickRbt={mOrderAscDsc}
           />
           
           <RButtonGroup
@@ -84,7 +96,7 @@ const Filter = () => {
             pId="iDsc"
             pLabel="Ord. Dsc" 
             pValue="DSC"
-            pMOnClickRbt={mOnClickRbtOrder}
+            pMOnClickRbt={mOrderAscDsc}
           />
         </MySection>
       </form> 
