dffdec3fee39177d81c902216aa41eb3789d25d1
JalzDeLezZ <j4mes.delez@gmail.com>
2022-04-26 16:34:58 -0500

    deploy5

--- client/src/redux/action.js
+++ client/src/redux/action.js
@@ -12,13 +12,15 @@ export const DELETE_VG_BY_ID = 'DELETE_VG_BY_ID';
 export const RELOAD_DATA = 'RELOAD_DATA';  
 export const GET_PLATFORMS = 'GET_PLATFORMS';  
 export const UPDATE_VIDEO_GAME = 'UPDATE_VIDEO_GAME';  
- 
+
+const URL_BASE = 'http://18.207.107.246:4001';
+// const URL_BASE = 'http://localhost:4001';
 
 export const getAllVideoGames =  (pQuery) => {
   return async (dispatch) => {
     try {
-      // const {data} = await axios.get(`http://18.207.107.246:3001/videogames?qry=${pQuery}`);
-      const {data} = await axios.get(`http://18.207.107.246:3001/videogames`);
+      // const {data} = await axios.get(`${URL_BASE}/videogames?qry=${pQuery}`);
+      const {data} = await axios.get(`${URL_BASE}/videogames`);
       dispatch({
         type: GET_ALL_VIDEOGAMES,
         payload: data
@@ -30,7 +32,7 @@ export const getAllVideoGames =  (pQuery) => {
 export const getVideoGameByName =  (pName) => {
   return async (dispatch) => {
     try {
-      const {data} = await axios.get(`http://18.207.107.246:3001/videogames?name=${pName}`);
+      const {data} = await axios.get(`${URL_BASE}/videogames?name=${pName}`);
       dispatch({
         type: GET_VIDEOGAMES_BY_NAME,
         payload: data
@@ -42,7 +44,7 @@ export const getVideoGameByName =  (pName) => {
 export const getDetailVideoGameById =  (pId) => {
   return async (dispatch) => {
     try {
-      const {data} = await axios.get(`http://18.207.107.246:3001/videogame/${pId}`);
+      const {data} = await axios.get(`${URL_BASE}/videogame/${pId}`);
       dispatch({
         type: GET_VIDEOGAMES_BY_ID,
         payload: data
@@ -54,7 +56,7 @@ export const getDetailVideoGameById =  (pId) => {
 export const getListGenres =  () => {
   return async (dispatch) => {
     try {
-      const {data} = await axios.get(`http://18.207.107.246:3001/genres`);
+      const {data} = await axios.get(`${URL_BASE}/genres`);
       dispatch({
         type: 'GET_LIST_GENRES',
         payload: data
@@ -66,7 +68,7 @@ export const getListGenres =  () => {
 export const postVideoGamesXGenres = (dataForm) => {
   return async () => {
     try {
-      const response = await axios.post(`http://18.207.107.246:3001/videogame`, dataForm);
+      const response = await axios.post(`${URL_BASE}/videogame`, dataForm);
       return response;
     } catch (error) { console.error(error);}
   }
@@ -92,7 +94,7 @@ export const FilterByDataAndGenre = (p1dataType, p2Genre) => {
 export const ActDeleteVG = (pIdentity) => {
   return async (dispatch) => {
     try {
-      const {data} = await axios.delete(`http://18.207.107.246:3001/videogame/${pIdentity}`);
+      const {data} = await axios.delete(`${URL_BASE}/videogame/${pIdentity}`);
       
       dispatch({
         type: DELETE_VG_BY_ID,
@@ -106,7 +108,7 @@ export const GetPlatforms = () => {
 
   return async (dispatch) => {
     try {
-      const {data} = await axios.get(`http://18.207.107.246:3001/platforms`)
+      const {data} = await axios.get(`${URL_BASE}/platforms`)
       dispatch({
         type: GET_PLATFORMS,
         payload: data
@@ -118,7 +120,7 @@ export const GetPlatforms = () => {
 export const UpdateVideoGame = (pIdentity, pDataForm) => {
   return async (dispatch) => {
     try {
-      const {data} = await axios.put(`http://18.207.107.246:3001/videogame/${pIdentity}`, pDataForm);
+      const {data} = await axios.put(`${URL_BASE}/videogame/${pIdentity}`, pDataForm);
       dispatch({
         type: UPDATE_VIDEO_GAME,
         payload: data
@@ -126,7 +128,7 @@ export const UpdateVideoGame = (pIdentity, pDataForm) => {
     } catch (error) { console.error(error);}
   }
 }
-  // const URL = `http://18.207.107.246:3001/platforms`
+  // const URL = `${URL_BASE}/platforms`
 
   // try {
   //   const {data} = await axios.get(URL);
